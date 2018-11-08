86f44f166d9b16eaa7e2e7aba42fa2237fe431e7
pina-mito <mito@pinnacles.tech>
2018-11-08 23:43:03 +0900

    クロスコンパイル環境作成

--- src/main.rs
+++ src/main.rs
@@ -2,8 +2,7 @@ extern crate ears;
 use ears::{AudioController, Sound};
 
 fn main() {
-    let mut sound = Sound::new("AbJo_Drum_and_Freak.wav").unwrap();
+    let mut sound = Sound::new("assets/sample.wav").unwrap();
     sound.play();
     while sound.is_playing() {};
 }
-
